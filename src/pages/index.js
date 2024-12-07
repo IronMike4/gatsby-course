560fd3ff30b6f42c14ac63acf8afdb5b7ed3528c
Tinago Mahachi <tinago.mahachi@gviprograms.com>
2024-12-07 18:51:55 +0000

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (<main><h1>this is the homepage</h1></main>);
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
