8296b03c54b0f4158b3a42cb683e83cd8e3f84b0
Tinago Mahachi <tinago.mahachi@gviprograms.com>
2024-12-08 20:25:39 +0000

    Advanced block rendering: Using Gatsby Image

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,8 @@
 const path = require("path");
-const { assignIds } = require("@webdeveducation/wp-block-tools");
+const {
+  assignIds,
+  assignGatsbyImage,
+} = require("@webdeveducation/wp-block-tools");
 const fs = require("fs");
 
 exports.createPages = async ({ actions, graphql }) => {
@@ -22,13 +25,14 @@ exports.createPages = async ({ actions, graphql }) => {
   `);
 
   try {
-    fs.writeFileSync("./public/themeStylesheet.css", data.wp.themeStylesheet)
+    fs.writeFileSync("./public/themeStylesheet.css", data.wp.themeStylesheet);
   } catch (e) {}
 
   for (let i = 0; i < data.allWpPage.nodes.length; i++) {
     const page = data.allWpPage.nodes[i];
     let blocks = page.blocks;
     blocks = assignIds(blocks);
+    blocks = await assignGatsbyImage({ blocks, graphql, coreMediaText: true });
     createPage({
       path: page.uri,
       component: pageTemplate,
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,9 +1,56 @@
 import React from "react";
-import { BlockRendererProvider } from "@webdeveducation/wp-block-tools";
+import {
+  BlockRendererProvider,
+  BlockRenderer,
+  getStyles,
+  getClasses,
+} from "@webdeveducation/wp-block-tools";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 const Page = (props) => {
-    console.log("PAGE PROPS", props);
-  return <BlockRendererProvider allBlocks={props.pageContext.blocks} />;
+  console.log("PAGE PROPS", props);
+  return (
+    <div>
+      {" "}
+      <BlockRendererProvider
+        allBlocks={props.pageContext.blocks}
+        renderComponent={(block) => {
+          switch (block.name) {
+            case "core/media-text": {
+              console.log("RENDER COMPONENT:", block);
+
+              const content = (
+                <div
+                 className={`flex p-4 ${
+                    block.attributes.verticalAlignment !== "center"
+                     ? "items-center"
+                      : ""
+                      }`}
+                      >
+                 <div>
+                    <BlockRenderer blocks={block.innerBlocks} />
+                    </div>
+                </div>
+              );
+              return (
+                <div
+                  key={block.id}
+                  style={getStyles(block)}
+                  className={getClasses(block)}
+                >
+                  {block.attributes.mediaPosition === "right" && content}
+                  <div>
+                    <GatsbyImage alt="" image={block.attributes.gatsbyImage} />
+                  </div>
+                  {block.attributes.mediaPosition !== "right" && content}
+                </div>
+              );
+            }
+          }
+        }}
+      />
+    </div>
+  );
 };
 
 export default Page;
